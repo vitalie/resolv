e1e0f69f84d863f45d5b53a48dc35db272a7801a
Vitalie Cherpec <vitalie@penguin.ro>
2015-04-06 09:34:04 +0300

    Return a channel

--- resolver.go
+++ resolver.go
@@ -1,6 +1,7 @@
 package resolv
 
 import (
+	"fmt"
 	"net"
 
 	"github.com/miekg/dns"
@@ -14,28 +15,45 @@ func NewResolver() *Resolver {
 	return &Resolver{}
 }
 
-func (r *Resolver) Do(ctx context.Context, req *Request) *Response {
-	m := new(dns.Msg)
-	m.SetQuestion(req.Name, req.Type)
+func (r *Resolver) Do(ctx context.Context, req *Request) <-chan *Response {
+	c := make(chan *Response, 1)
 
-	c := new(dns.Client)
-	// TODO: https://godoc.org/github.com/miekg/dns#Exchange
-	// - UDP/TCP
-	// - Truncated response
-	// - Timeout
-	// - Retry
-	in, rtt, err := c.Exchange(m, req.Addr)
-	if err != nil {
-		return &Response{Err: err}
-	}
+	go func() {
+		defer func() {
+			// Unhandled error
+			if err := recover(); err != nil {
+				c <- &Response{Err: fmt.Errorf("resolve: %v", err)}
+			}
+			close(c)
+		}()
 
-	if in.Rcode != dns.RcodeSuccess {
-		err := &net.DNSError{Err: dns.RcodeToString[in.Rcode], Name: req.Name, Server: req.Addr}
-		return &Response{Err: err}
-	}
+		// Prepare message
+		m := new(dns.Msg)
+		m.SetQuestion(req.Name, req.Type)
 
-	return &Response{
-		Msg: in,
-		Rtt: rtt,
-	}
+		cli := new(dns.Client)
+		// TODO: https://godoc.org/github.com/miekg/dns#Exchange
+		// - UDP/TCP
+		// - Truncated response
+		// - Timeout
+		// - Retry
+		in, rtt, err := cli.Exchange(m, req.Addr)
+		if err != nil {
+			c <- &Response{Err: err}
+			return
+		}
+
+		if in.Rcode != dns.RcodeSuccess {
+			err := &net.DNSError{Err: dns.RcodeToString[in.Rcode], Name: req.Name, Server: req.Addr}
+			c <- &Response{Err: err}
+			return
+		}
+
+		c <- &Response{
+			Msg: in,
+			Rtt: rtt,
+		}
+	}()
+
+	return c
 }
