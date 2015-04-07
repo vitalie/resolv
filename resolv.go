511bcebc6c8eeae931323bf7997a92d5bcad7512
Vitalie Cherpec <vitalie@penguin.ro>
2015-04-07 16:22:43 +0300

    Remove unused constants

--- resolv.go
+++ resolv.go
@@ -2,17 +2,6 @@ package resolv
 
 const (
 	PortDefault = "53"
-
-	ProtoUDP = "udp"
-	ProtoTCP = "tcp"
-)
-
-type Mode int
-
-const (
-	ModeUDP Mode = 1 << iota
-	ModeTCP
-	ModeMixed
 )
 
 var RootServers = []string{
