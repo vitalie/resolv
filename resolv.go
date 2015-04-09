b1ee2a0a79d653af22ef3c8c6dcf80d3ba8c454e
Vitalie Cherpec <vitalie@penguin.ro>
2015-04-09 10:51:26 +0300

    Add i-m root servers

--- resolv.go
+++ resolv.go
@@ -14,4 +14,8 @@ var RootServers = []string{
 	"g.root-servers.net.",
 	"h.root-servers.net.",
 	"i.root-servers.net.",
+	"j.root-servers.net.",
+	"k.root-servers.net.",
+	"l.root-servers.net.",
+	"m.root-servers.net.",
 }
