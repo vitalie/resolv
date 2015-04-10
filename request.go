8b003b87a7c4cb20dc134703878e18aeb4bc417a
Vitalie Cherpec <vitalie@penguin.ro>
2015-04-10 08:36:24 +0300

    Update comments

--- request.go
+++ request.go
@@ -7,20 +7,32 @@ import (
 	"github.com/miekg/dns"
 )
 
+// Request represents a DNS request.
 type Request struct {
-	Addr  string
-	Mode  string
-	Name  string
-	Type  uint16
-	Class uint16
+	Addr    string // Remote host:port
+	Mode    string // Mode tcp/udp
+	Name    string // Query name
+	Type    uint16 // Query type
+	Class   uint16 // Query class
+	Recurse bool   // Recursion desired
 }
 
+// RequestOption represents an option function.
 type RequestOption func(*Request)
 
+// SetTCPMode configures the query to run in TCP mode.
 func SetTCPMode(req *Request) {
 	req.Mode = "tcp"
 }
 
+// SetRD enables recursion for a request.
+func SetRD(recurse bool) RequestOption {
+	return func(req *Request) {
+		req.Recurse = recurse
+	}
+}
+
+// SetCHAOSClass sets the class to CHAOS for a request.
 func SetCHAOSClass(req *Request) {
 	req.Class = dns.ClassCHAOS
 }
