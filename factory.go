19ed3f693191c137582c5293a73cc85a4b76a03d
Vitalie Cherpec <vitalie@penguin.ro>
2015-04-10 10:15:01 +0300

    Update RequestFactory comments

--- factory.go
+++ factory.go
@@ -1,16 +1,18 @@
 package resolv
 
-// RequestFactory creates multiple requests using opts.
+// RequestFactory creates multiple requests using custom options.
 type RequestFactory struct {
 	opts []RequestOption
 }
 
+// NewRequestFactory creates a factory using custom options.
 func NewRequestFactory(opts ...RequestOption) *RequestFactory {
 	return &RequestFactory{
 		opts: opts,
 	}
 }
 
+// FromNames creates multiple requests using different names.
 func (f *RequestFactory) FromNames(addr string, type_ uint16, names ...string) []*Request {
 	var reqs []*Request
 
@@ -22,6 +24,7 @@ func (f *RequestFactory) FromNames(addr string, type_ uint16, names ...string) [
 	return reqs
 }
 
+// FromTypes creates multiple requests using different types.
 func (f *RequestFactory) FromTypes(addr, name string, types ...uint16) []*Request {
 	var reqs []*Request
 
